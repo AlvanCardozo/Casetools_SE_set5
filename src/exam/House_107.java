166c1489b1d2dee4eddb5a782cf1f956dd5ea35e
exam <exam@Bcalab4-m12>
2024-04-27 09:15:39 +0530

    commit1

--- src/exam/House_107.java
+++ src/exam/House_107.java
@@ -1,5 +1,14 @@
 package exam;
 
 public class House_107 {
+	String color;
+	String Owner;
+	House_107(String color,String Owner ){
+		this.color=color;
+		this.Owner=Owner;
+	}
+	String getOwner() {
+		return Owner;
+	}
 
 }
